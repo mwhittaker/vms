e0e557c306b8737ae91d6795de6a682e16b5548e
Michael Whittaker <mjw297@cornell.edu>
2016-01-09 15:39:50 -0500

    Added DB VM with postgres.

---
+++ db/install_postgres.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+set -euo pipefail
+
+# Postgres Installation.
+# http://www.postgresql.org/download/linux/ubuntu/
+sudo bash -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+    sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-9.5 postgresql-server-dev-9.5 python-dev
+
+# Postgres Setup.
+#
+# By default, a fresh postgres installation has a single role: postgres. In
+# order to access the database as the vagrant user, we have to create a role as
+# postgres for vagrant. Then we create a database named vagrant as vagrant.
+#
+# http://www.postgresql.org/docs/current/interactive/database-roles.html
+sudo su postgres -c 'createuser vagrant -s'
+createdb vagrant
+
+# pgcli Installation
+sudo pip install pgcli
