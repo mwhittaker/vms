b27cc439ac590e8f0bbf308bc0a4698a4c84cf6a
Michael Whittaker <mjw297@cornell.edu>
2016-09-21 17:46:02 -0700

    Added erlang VM.

---
+++ erlang/install_erlang.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Resources:
+#   - https://www.erlang-solutions.com/resources/download.html
+
+main() {
+    wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+    sudo dpkg -i erlang-solutions_1.0_all.deb
+    sudo apt-get update
+    sudo apt-get install erlang
+}
+
+main
