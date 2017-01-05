c6059620ba962ed5b8fbbe30e4b32807ef0f837a
Michael Whittaker <mjw297@cornell.edu>
2017-01-04 19:46:52 -0800

    Added VM for Istvan.

---
+++ istvan/install_felix.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Links:
+#  - https://github.com/frenetic-lang/frenetic/tree/verification_and_felix
+#  - https://github.com/frenetic-lang/frenetic-vm
+#  - https://github.com/frenetic-lang/netkat-automata
+
+set -euo pipefail
+
+main() {
+    sudo apt-get install -y g++ autoconf debianutils zlib1g-dev
+    git clone git@github.com:frenetic-lang/frenetic.git --branch verification_and_felix
+    opam pin add frenetic frenetic -n -k git
+    opam install -y opam_extended frenetic
+
+    # cd frenetic
+    # make
+    # ./frenetic.native shell
+    # felix <in> <p> <t> <out> <q>
+}
+
+main
