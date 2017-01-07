d772be2634cbae79ee3992008aa21432d8bab19c
Michael Whittaker <mjw297@cornell.edu>
2017-01-07 13:44:03 -0800

    Install erlang without prompt.

--- erlang/install_erlang.sh
+++ erlang/install_erlang.sh
@@ -7,7 +7,7 @@ main() {
     wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
     sudo dpkg -i erlang-solutions_1.0_all.deb
     sudo apt-get update
-    sudo apt-get install erlang
+    sudo apt-get install -y erlang
 }
 
 main
