970ec3ed3f84d92afc2ff785d27b69e9362ba12d
Michael Whittaker <mjw297@cornell.edu>
2017-04-19 13:43:26 -0700

    Updated C++ VM.

--- cpp/README.md
+++ cpp/README.md
@@ -1,8 +1,6 @@
 ```bash
 bash /vagrant/install_dotfiles.sh
 bash /vagrant/install_clang.sh
+bash /vagrant/install_gpp.sh
 bash /vagrant/install_cmake.sh
-bash /vagrant/install_tbb.sh
-bash /vagrant/install_zeromq.sh
-bash /vagrant/install_protobuf.sh
 ```
--- cpp/install_clang.sh
+++ cpp/install_clang.sh
@@ -9,16 +9,22 @@
 set -euo pipefail
 
 main() {
-    sudo apt-get install -y \
-        clang-3.8 clang-format-3.8 clang-tidy-3.8 lldb-3.8 libc++-dev libc++abi-dev
-    sudo ln -s "$(which clang-3.8)" /usr/bin/clang
-    sudo ln -s "$(which clang++-3.8)" /usr/bin/clang++
-    sudo ln -s "$(which clang-format-3.8)" /usr/bin/clang-format
-    sudo ln -s "$(which clang-tidy-3.8)" /usr/bin/clang-tidy
-    sudo ln -s "$(which lldb-3.8)" /usr/bin/lldb
-    sudo ln -s "$(which lldb-server-3.8)" /usr/bin/lldb-server
+    sudo bash -c "echo '' >> /etc/apt/sources.list"
+    sudo bash -c "echo '# http://apt.llvm.org/' >> /etc/apt/sources.list"
+    sudo bash -c "echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main' >> /etc/apt/sources.list"
+    sudo bash -c "echo 'deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main' >> /etc/apt/sources.list"
+    wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
 
-    # TODO(mwhittaker): Install clang-include-fixer, asan, tsan, and msan.
+    sudo apt-get install -y \
+        clang-3.9 clang-format-3.9 clang-tidy-3.9 lldb-3.9
+    sudo ln -s "$(which clang-3.9)" /usr/bin/clang
+    sudo ln -s "$(which clang++-3.9)" /usr/bin/clang++
+    sudo ln -s "$(which clang-format-3.9)" /usr/bin/clang-format
+    sudo ln -s "$(which clang-tidy-3.9)" /usr/bin/clang-tidy
+    sudo ln -s "$(which lldb-3.9)" /usr/bin/lldb
+    sudo ln -s "$(which lldb-server-3.9)" /usr/bin/lldb-server
 }
 
 main
