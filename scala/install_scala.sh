a1e824be8e3692bb282044adb24908125a6df5be
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-09-22 00:09:16 -0700

    Added scala VM.

---
+++ scala/install_scala.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    # http://www.scala-sbt.org/0.13/docs/Installing-sbt-on-Linux.html
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    sudo apt-get update
+    sudo apt-get install sbt
+}
+
+main
