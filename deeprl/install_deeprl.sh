23ecc2cd3019928e9ad1c2b6edd65ae91fde48b2
Michael Whittaker <mjwhittaker@berkeley.edu>
2017-09-28 19:44:17 -0700

    Updated deeprl install.

--- deeprl/install_deeprl.sh
+++ deeprl/install_deeprl.sh
@@ -3,17 +3,12 @@
 set -euo pipefail
 
 main() {
-    # Install tensorflow (https://www.tensorflow.org/install/install_linux#InstallingAnaconda).
-    pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.3.0-cp35-cp35m-linux_x86_64.whl
-
-    # Install OpenAI gym.
-    pip install --upgrade gym
-
-    # Install mujoco-py. See [1] for dependency information.
+    # Install dependencies See [1] for dependency information.
     # [1]: https://github.com/openai/mujoco-py/blob/master/Dockerfile
     sudo apt-get install \
         cmake \
         curl \
+        g++ \
         gfortran \
         libatlas-dev \
         libblas-dev \
@@ -31,13 +26,24 @@ main() {
         net-tools \
         unzip \
         wget \
+        libav-tools \
         xorg-dev \
         xpra \
         xserver-xorg-dev \
-        xvfb
+        xvfb \
+        zlib1g-dev
+
+    # Install tensorflow (https://www.tensorflow.org/install/install_linux#InstallingAnaconda).
+    pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.3.0-cp35-cp35m-linux_x86_64.whl
+
+    # Install OpenAI gym.
+    pip install --upgrade gym
+
+    # Install mujoco-py.
     pip install --verbose --upgrade mujoco-py==0.5.7
 
-    pip install scipy seaborn
+    # Install misc.
+    pip install scipy seaborn opencv-python 'gym[atari]'
 }
 
 main
