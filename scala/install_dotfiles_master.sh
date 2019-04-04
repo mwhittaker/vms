b3fffa214600ccad6c7c526a137e621b28d2d74b
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-04-04 14:51:08 -0700

    Updated scala VM.

--- scala/install_dotfiles_master.sh
+++
@@ -1,1 +0,0 @@
-../install_dotfiles_master.sh
--- scala/install_scala.sh
+++ scala/install_scala.sh
@@ -1,18 +1,1 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-main() {
-    mkdir -p "$HOME/install"
-    cd "$HOME/install"
-
-    # As described in [1], we install scala by installing sbt.
-    #
-    # [1]: https://www.scala-lang.org/download/
-    local -r version="1.1.6"
-    wget "https://piccolo.link/sbt-${version}.tgz"
-    tar -xzvf "sbt-${version}.tgz"
-    echo 'export PATH="$PATH:$HOME/install/sbt/bin"' >> ~/.bash_path
-}
-
-main "$@"
+../install_scala.sh
