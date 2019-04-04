9ff50edbfdbda5edd11928faab20143bfe4eef36
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-04-04 14:51:25 -0700

    Updated typescript VM.

--- typescript/install_nodejs.sh
+++ typescript/install_nodejs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 main() {
     # https://nodejs.org/en/download/package-manager/
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     sudo apt-get install -y build-essential nodejs
 }
 
