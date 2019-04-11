518b46f9e06533a67a30e4cce17fa88fc81ba2fa
Michael Whittaker <mjwhittaker@berkeley.edu>
2019-04-11 23:01:59 +0000

    Fixed launch_daemons script.

--- frankenpaxos/launch_daemons.sh
+++ frankenpaxos/launch_daemons.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# Get the `trun` command.
+source ~/.bash_functions
+
 set -euo pipefail
 
 main() {
@@ -7,9 +10,9 @@ main() {
     trun -s daemons \
         'jupyter notebook --no-browser --ip 0.0.0.0 --port 8000' \
         'python -m http.server 8001' \
-        'source activate flamescope && python run.py' \
-        '# prometheus --config.file=prometheus.yml --storage.tsdb.path=prometheus_data --web.listen-address=0.0.0.0:8003' \
-        'GF_SERVER_HTTP_PORT=8004 GF_PATHS_PROVISIONING=~/frankenpaxos/grafana ./bin/grafana-server web'
+        'cd flamescope && source activate flamescope && python run.py' \
+        'cd prometheus-* # prometheus --config.file=prometheus.yml --storage.tsdb.path=prometheus_data --web.listen-address=0.0.0.0:8003' \
+        'cd grafana-* && GF_SERVER_HTTP_PORT=8004 GF_PATHS_PROVISIONING=~/frankenpaxos/grafana ./bin/grafana-server web'
 }
 
 main "$@"
